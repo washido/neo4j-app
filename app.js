22743f5153c864a802f14a3e9393a8e30ecf7a64
Icaro Heimig <icaro@icaromh.com>
2016-07-26 15:59:57 -0300

    Removed logger

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express      = require('express');
 var path         = require('path');
 var favicon      = require('serve-favicon');
-var logger       = require('morgan');
+// var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
-var session      = require('express-session'); 
+var session      = require('express-session');
 
 var routes       = require('./routes/index');
 var user         = require('./routes/user');
@@ -20,7 +20,7 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
